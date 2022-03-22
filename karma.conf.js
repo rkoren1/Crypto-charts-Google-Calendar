b5861ca097360afe73a618b04dbdd45220ae750e
rkoren1 <korenrok@hotmail.com>
2022-03-22 09:27:18 +0100

    renamed project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/SalesDashboardv2'),
+      dir: require('path').join(__dirname, './coverage/DemoApp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
